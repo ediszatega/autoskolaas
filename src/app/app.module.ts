7d504b43342271b42b76c95f34452806223b8876
adnan.bubalo <bubalo.adnan@gmail.com>
2023-02-07 22:26:11 +0100

    Add test module component and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AboutUsComponent } from './components/about-us/about-us.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/common/navbar/navbar.component';
 import { FooterComponent } from './components/common/footer/footer.component';
+import { TestModuleComponent } from './components/test-module/test-module.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FooterComponent } from './components/common/footer/footer.component';
     HomeComponent,
     NavbarComponent,
     FooterComponent,
+    TestModuleComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
