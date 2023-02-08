5d5a1485d22cb9e8891042b272c0df740e0ec033
adnan.bubalo <bubalo.adnan@gmail.com>
2023-02-08 05:03:32 +0100

    Add test module logic and test 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { TestModuleComponent } from './components/test-module/test-module.component';
 
-const routes: Routes = [{ path: 'o-nama', component: AboutUsComponent }];
+const routes: Routes = [
+  { path: 'o-nama', component: AboutUsComponent },
+  { path: 'testovi', component: TestModuleComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
