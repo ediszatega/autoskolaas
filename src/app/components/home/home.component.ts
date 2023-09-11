4d2d839470ef15cd28276cbf8ff7f1f67e6eebf0
Edis Zatega <ediszatega@hotmail.com>
2023-09-11 18:06:32 +0200

    home page add content

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,8 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  more_less: boolean = false;
+  more_less_label: string = 'Prikaži više';
+
   ngOnInit(): void {
     window.scrollTo(0, 0);
+  }
+
+  readMoreLess() {
+    if (this.more_less === false) {
+      this.more_less_label = 'Prikaži manje';
+    } else {
+      this.more_less_label = 'Prikaži više';
+    }
+    this.more_less = !this.more_less;
   }
 
   questions = document.querySelectorAll('.questions-answers');
