a4a6a1e06d04270a186abe516f097d62ed4dbbc8
Edis Zatega <ediszatega@hotmail.com>
2023-10-01 23:30:42 +0200

    add category selection for tests

--- src/app/components/test-module/test-selection/test-selection.component.ts
+++ src/app/components/test-module/test-selection/test-selection.component.ts
@@ -7,28 +7,43 @@ import { Router } from '@angular/router';
   styleUrls: ['./test-selection.component.css'],
 })
 export class TestSelectionComponent {
+  selectedCategory: string = 'KATEGORIJA B';
   selectedTest: string = 'Test 1';
-  tests: string[] = [
-    'Test 1',
-    'Test 2',
-    'Test 3',
-    'Test 4',
-    'Test 5',
-    'Test 6',
-    'Test 7',
-    'Test 8',
-    'Test 9',
-    'Test 10',
-    'Test 11',
-    'Test 12',
-    'Test 13',
-    'Test 14',
-    'Test 15',
-    'Test 16',
-    'Test 17',
-    'Test 18',
-    'Test 19',
+
+  categories: string[] = [
+    'Kategorija A',
+    'Kategorija B',
+    'Kategorija C',
+    'Kategorija D',
   ];
+
+  // Define a map of categories to tests
+  categoryToTests: { [key: string]: string[] } = {
+    'Kategorija A': ['Test 1'],
+    'Kategorija B': [
+      'Test 1',
+      'Test 2',
+      'Test 3',
+      'Test 4',
+      'Test 5',
+      'Test 6',
+      'Test 7',
+      'Test 8',
+      'Test 9',
+      'Test 10',
+      'Test 11',
+      'Test 12',
+      'Test 13',
+      'Test 14',
+      'Test 15',
+      'Test 16',
+      'Test 17',
+      'Test 18',
+      'Test 19',
+    ],
+    'Kategorija C': ['Test 1'],
+    'Kategorija D': ['Test 1'],
+  };
 
   constructor(private router: Router) {}
 
